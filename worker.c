b2c76841c5b8ad7c9173de89c6cc00aff46c3108
Aaron Hsu <aaronhsu4606@gmail.com>
2021-12-09 19:03:08 -0500

    push to comp

--- worker.c
+++ worker.c
@@ -1,9 +1,16 @@
-#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
+#include <time.h>
 
 int toupper(int c) {
   if (c >= 'a' && c <= 'z') {
